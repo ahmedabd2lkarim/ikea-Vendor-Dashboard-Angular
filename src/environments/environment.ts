58b077dead4492c22dfaf51f878c0da0a21d61f7
MagySamwel <maggi1samwel@gmail.com>
2025-06-04 12:56:16 +0300

    edit in order details

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: true,
-  baseURL: 'https://ikea-api-dqtp.onrender.com',
+  // baseURL: 'https://ikea-api-dqtp.onrender.com',
+    baseURL:'http://localhost:5000',
+
 };
